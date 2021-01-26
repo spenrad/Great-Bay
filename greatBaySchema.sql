ad4e00284dc41b703072eefc69b913c987147a52
Peter Ting <pting1995@gmail.com>
2021-01-26 13:25:50 -0800

    added values into greenBay_DB

--- greatBaySchema.sql
+++ greatBaySchema.sql
@@ -12,3 +12,17 @@ CREATE TABLE auctions(
   PRIMARY KEY (id)
 );
 
+INSERT INTO auctions (item_name, category, starting_bid, highest_bid)
+VALUES ("Eggs", "food", "1", "10");
+INSERT INTO auctions (item_name, category, starting_bid, highest_bid)
+VALUES ("Cheese", "food", "2", "5");
+INSERT INTO auctions (item_name, category, starting_bid, highest_bid)
+VALUES ("Tomato", "food", "7", "7");
+INSERT INTO auctions (item_name, category, starting_bid, highest_bid)
+VALUES ("Onion", "food", "1", "5");
+INSERT INTO auctions (item_name, category, starting_bid, highest_bid)
+VALUES ("steering wheel", "car", "5", "100");
+INSERT INTO auctions (item_name, category, starting_bid, highest_bid)
+VALUES ("gas pedal", "car", "100", "110");
+
+SELECT * FROM auctions;
